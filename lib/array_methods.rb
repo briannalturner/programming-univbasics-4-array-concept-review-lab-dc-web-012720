0a69b9782d447a58b6d4f1e1d1e53814bbe42244
Bri Turner <blturner3527@gmail.com>
2020-01-09 16:49:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times {
     |index|
-    if array[index] = value_to_find
-      index
-    else 
-      nil
+    if array[index] == value_to_find
+      return index
+  end
   }
 end
 
